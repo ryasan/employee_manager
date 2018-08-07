68b1a54656899c3354d46211b54f367cc147e9b9
Ryan Santos <ryansantos86@gmail.com>
2018-08-07 16:11:52 -0700

    1.0

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,8 @@
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './../actions/types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
+} from './../actions/types';
 const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
 export default (state = INITIAL_STATE, action) => {
@@ -6,6 +10,8 @@ export default (state = INITIAL_STATE, action) => {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
       return INITIAL_STATE;
     default:
       return state;
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
+
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 class RouterComponent extends Component {
   render() {
@@ -30,6 +32,11 @@ class RouterComponent extends Component {
               key="employeeCreate"
               component={EmployeeCreate}
               title="Create Employee"
+            />
+            <Scene
+              key="employeeEdit"
+              component={EmployeeEdit}
+              title="Edit Employee"
             />
           </Scene>
         </Scene>
