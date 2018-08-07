ae2b2b38318eee5dba9f284544e8b55e7899c9ca
Ryan Santos <ryansantos86@gmail.com>
2018-08-06 23:51:54 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+import Router from './router';
+import { firebaseConfig } from './config/config';
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
